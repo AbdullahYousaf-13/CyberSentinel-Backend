442c1e96fda03022b14b40084e86b391c83d2987
Abdullah Yousaf <abdullahyousaf132@gmail.com>
2026-01-03 22:43:06 +0500

    backend basic setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('CyberSentinel Backend is Running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
